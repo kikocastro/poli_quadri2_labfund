44dc19dc5e648ec9428c3e56afbdee51e214ee63
Frederico Castro <castro.112358@gmail.com>
2015-10-29 20:03:09 -0200

    A08 almost there

---
+++ T3G14A08/T3G14A08E03/mvn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# EX1
+# Montador
+java -cp MLR.jar montador.MvnAsm T3G14A08E03_main.asm
+java -cp MLR.jar montador.MvnAsm T3G14A08E03_const.asm
+java -cp MLR.jar montador.MvnAsm T3G14A08E03_rotinas.asm
+
+# Linker
+java -cp MLR.jar linker.MvnLinker T3G14A08E03_main.mvn T3G14A08E03_const.mvn T3G14A08E03_rotinas.mvn -s T3G14A08E03_final.mvn
+
+# Relocador
+java -cp MLR.jar relocator.MvnRelocator T3G14A08E03_final.mvn o.mvn 0000 0000
