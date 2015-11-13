591e2c9f10612f7542aadda02edc297a7f4307fc
Frederico Castro <castro.112358@gmail.com>
2015-11-13 13:02:44 -0200

    removed prog10 from linked program

--- T3G14A10/mvn.sh
+++ T3G14A10/mvn.sh
@@ -5,10 +5,9 @@
 java -cp MLR.jar montador.MvnAsm T3G14A10E01_main.asm
 java -cp MLR.jar montador.MvnAsm T3G14A10E01_const.asm
 java -cp MLR.jar montador.MvnAsm T3G14A10E01_rotinas.asm
-java -cp MLR.jar montador.MvnAsm prog10.asm
 
 # Linker
-java -cp MLR.jar linker.MvnLinker T3G14A10E01_main.mvn T3G14A10E01_const.mvn T3G14A10E01_rotinas.mvn -s T3G14A10E01_final.mvn prog10.mvn
+java -cp MLR.jar linker.MvnLinker T3G14A10E01_main.mvn T3G14A10E01_const.mvn T3G14A10E01_rotinas.mvn -s T3G14A10E01_final.mvn
 
 # Relocador
 java -cp MLR.jar relocator.MvnRelocator T3G14A10E01_final.mvn o.mvn 0000 0000
